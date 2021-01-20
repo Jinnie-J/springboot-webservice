f9bcb40947234cb1f310b73a7d0b2b895102519a
Jinnie-J <jjesweet@naver.com>
2021-01-20 12:49:57 +0900

    세션 어노테이션 설정

--- src/main/java/com/web/springboot/web/IndexController.java
+++ src/main/java/com/web/springboot/web/IndexController.java
@@ -1,12 +1,12 @@
 package com.web.springboot.web;
 
+import com.web.springboot.config.auth.LoginUser;
 import com.web.springboot.config.auth.dto.SessionUser;
 import com.web.springboot.service.posts.PostsService;
 import com.web.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -20,13 +20,14 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts",postsService.findAllDesc());
-        SessionUser user= (SessionUser) httpSession.getAttribute("user");
-
-        if( user!=null ){
+        if (user != null){
             model.addAttribute("userName", user.getName());
+            System.out.println("login");
         }
+        else
+            System.out.println("loginfail");
         return "index";
     }
 
