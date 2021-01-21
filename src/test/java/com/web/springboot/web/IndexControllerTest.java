dd48f21c1ee1472378ea4b53f136fd7fba840ec9
Jinnie-J <jjesweet@naver.com>
2021-01-21 17:17:23 +0900

    text코드 오류수정

--- src/test/java/com/web/springboot/web/IndexControllerTest.java
+++ src/test/java/com/web/springboot/web/IndexControllerTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
@@ -18,10 +19,11 @@ public class IndexControllerTest {
     private TestRestTemplate restTemplate;
 
     @Test
-    public void 메인페이지_로딩(){
+    public void 메인페이지_로딩() {
+        //when
+        String body = this.restTemplate.getForObject("/", String.class);
 
-        String body=this.restTemplate.getForObject("/", String.class);
-
-        assertThat(body).contains("스프링 부트로 시작하는 웹 서비스");
+        //then
+        assertThat(body).contains("스프링부트로 시작하는 웹 서비스");
     }
 }
